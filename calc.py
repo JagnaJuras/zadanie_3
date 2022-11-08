b1912b88492c348f07f145f3da9a4e979ca6b7ce
JagnaJuras <116656769+JagnaJuras@users.noreply.github.com>
2022-11-08 01:26:14 +0100

    Added calc.py and test_calc.py

---
+++ calc.py
@@ -0,0 +1,11 @@
+
+def div_numbers(a, b):
+    return a / b
+
+
+def mul_numbers(a, b):
+    return a * b
+
+
+def add_numbers(a, b):
+    return a + b
---
+++ test_calc.py
@@ -0,0 +1,16 @@
+import calc
+
+
+class TestCalc:
+
+    def test_div_numbers(self):
+        result = calc.div_numbers(10, 5)
+        assert result == 2
+
+    def test_mul_numbers(self):
+        result = calc.mul_numbers(10, 5)
+        assert result == 50
+
+    def test_add_numbers(self):
+        result = calc.add_numbers(10, 5)
+        assert result == 15
